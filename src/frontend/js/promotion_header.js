02d2ab2e18cf38c9ffbf53b3baf8b61e172340e6
Anil Keshari <@groupon.com>
2016-07-24 02:04:40 +0530

    Clean up

--- src/frontend/js/promotion_header.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var $ = require("jquery");
-$(".promotion-header .close-icon").on("click", function () {
-    $(".promotion-header").slideUp();
-});
-//# sourceMappingURL=promotion_header.js.map
