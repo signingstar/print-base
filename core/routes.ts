01ed16f42789bb2ce5eb23667f02c7bb83c68524
Anil <anilkeshari@gmail.com>
2016-07-25 21:39:28 +0530

    Add cart icon. Add session pages (#4)

--- core/routes.ts
+++ core/routes.ts
@@ -1,13 +1,16 @@
 var path = require('path');
 var pug = require('pug');
 
-var srcPath = './src/templates/main.pug';
-var navigationConfig = require('../core/navigation-config.json');
+const templatePath:string = './src/templates';
+let srcPath:string = '';
 
 export function routes(app:any, staticCall:any) {
 	app.set("view engine", "pug");
 
 	app.get("/", function(req:any, res:any){
+		srcPath = `${templatePath}/main.pug`;
+		var navigationConfig = require('../core/navigation-config.json');
+
 		var fn = pug.compileFile(srcPath , {cache: false, pretty: true});
 		var navigationConfig = require('../core/navigation-config.json');
 
@@ -22,5 +25,34 @@ export function routes(app:any, staticCall:any) {
 		res.write(html);
 		res.end();
 	});
+
+	app.get("/login", function(req:any, res:any){
+		srcPath = `${templatePath}/session/login.pug`;
+
+		var fn = pug.compileFile(srcPath , {cache: false, pretty: true});
+		var html = fn();
+
+		res.writeHead(200, {
+			"Content-Type": "text/html"
+		});
+
+		res.write(html);
+		res.end();
+	});
+
+	app.get("/signup", function(req:any, res:any){
+		srcPath = `${templatePath}/session/signup.pug`;
+
+		var fn = pug.compileFile(srcPath , {cache: false, pretty: true});
+		var html = fn();
+
+		res.writeHead(200, {
+			"Content-Type": "text/html"
+		});
+
+		res.write(html);
+		res.end();
+	});
+
 	app.use('/assets', staticCall('./public'));
 }
