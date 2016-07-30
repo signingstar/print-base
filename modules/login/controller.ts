fecb2d18b59ac8b0df67184a8f78bcb95d2bf8ec
Anil <anilkeshari@gmail.com>
2016-07-31 00:04:04 +0530

    Link Menu nav to floating panel. Session buttons fix (#15)

--- modules/login/controller.ts
+++ modules/login/controller.ts
@@ -1,9 +1,16 @@
 let pug = require('pug');
-let path = require('path');
 
 module.exports = function(){
   return {
-    main: function({attributes, responders, page}) {
+    login_form: function({attributes, responders, page}) {
+      let {req, res} = attributes;
+    	let srcPath:string = './modules/login/main.pug';
+      let fn = pug.compileFile(srcPath , {cache: false, pretty: true});
+
+      responders.html(fn());
+    },
+
+    login_action: function({attributes, responders, page}) {
       let {req, res} = attributes;
     	let srcPath:string = './modules/login/main.pug';
       let fn = pug.compileFile(srcPath , {cache: false, pretty: true});
