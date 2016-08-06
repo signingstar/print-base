e592369409391c2c005ebc1c470168cc4909e759
Anil Keshari <@groupon.com>
2016-08-06 19:43:18 +0530

    Refactor right panel

---
+++ modules/order/frontend/components/floating_panel.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { PrintData } from "../presenter";
+
+export class FloatingPanel extends React.Component<any, any> {
+  constructor() {
+    super();
+
+    this.clearState = this.clearState.bind(this);
+  }
+
+  clearState() {
+    this.props.store.dispatch({type: 'clear'});
+  }
+
+  render () {
+    const value = this.props.store.getState();
+
+    let stateLabelsMap = {
+      type: value.type ? PrintData['type'][value.type].label : undefined,
+      size: value.size ? PrintData['size'][value.size].label : undefined,
+      material: value.material ? PrintData['material'][value.material].label : undefined,
+      quantity: value.quantity ? PrintData['quantity'][value.quantity].label : undefined
+    };
+
+    return (
+      <div className='right-panel-content'>
+        <div className='header'><h4>Order Summary </h4></div>
+        <ul>
+          <li className="visiting-cards-nav" id="visiting-cards-nav">Print Type: <span>{stateLabelsMap.type}</span></li>
+          <li className="stationary-nav">Print Size: <span>{stateLabelsMap.size}</span></li>
+          <li className="brouchers-nav">Print Material: <span>{stateLabelsMap.material}</span></li>
+          <li className="brouchers-nav">Print Quantity: <span>{stateLabelsMap.quantity}</span></li>
+        </ul>
+        <div className='estimates'><h5>Estimated Price:</h5></div>
+        <div className='clear'><a href='#' onClick={this.clearState}>clear </a></div>
+      </div>
+    );
+  }
+}
--- modules/order/frontend/components/section_body.tsx
+++ modules/order/frontend/components/section_body.tsx
@@ -3,8 +3,8 @@ import { PrintType } from "./print_type";
 import { PrintSize} from "./print_size";
 import { SelectMaterials } from "./select_materials";
 import { PrintQuantity } from "./print_quantity";
-import { PrintData } from "../presenter";
 import { StoreType } from "../store_type";
+import { FloatingPanel } from "./floating_panel";
 
 interface PropTypes {
   store: StoreType;
@@ -16,12 +16,7 @@ export class SectionBody extends React.Component<PropTypes, any> {
   constructor() {
     super();
 
-    this.clearState = this.clearState.bind(this);
     this.updateState = this.updateState.bind(this);
-  }
-
-  clearState() {
-    this.store.dispatch({type: 'clear'});
   }
 
   updateState(id: string, value: string) {
@@ -33,13 +28,6 @@ export class SectionBody extends React.Component<PropTypes, any> {
     this.store = this.props.store;
     const value = this.store.getState();
 
-    let stateLabelsMap = {
-      type: value.type ? PrintData['type'][value.type].label : undefined,
-      size: value.size ? PrintData['size'][value.size].label : undefined,
-      material: value.material ? PrintData['material'][value.material].label : undefined,
-      quantity: value.quantity ? PrintData['quantity'][value.quantity].label : undefined
-    };
-
     return (
       <div className='main-section-body'>
         <div className='left-panel'>
@@ -49,17 +37,8 @@ export class SectionBody extends React.Component<PropTypes, any> {
           <PrintQuantity selectedItem={value.quantity}  id='print-quantity' onChange={this.updateState} states={value} />
         </div>
         <div className='right-panel'>
-          <div className='right-panel-content'>
-            <div className='header'><h4>Order Summary </h4></div>
-            <ul>
-              <li className="visiting-cards-nav" id="visiting-cards-nav">Print Type: <span>{stateLabelsMap.type}</span></li>
-              {stateLabelsMap.size ? <li className="stationary-nav" id="stationary-nav">Print Size: <span>{stateLabelsMap.size}</span></li> : null}
-              {stateLabelsMap.material ? <li className="brouchers-nav" id="brouchers-nav">Print Material: <span>{stateLabelsMap.material}</span></li> : null}
-              {stateLabelsMap.quantity ? <li className="brouchers-nav" id="brouchers-nav">Print Quantity: <span>{stateLabelsMap.quantity}</span></li> : null}
-            </ul>
-            <div className='estimates'><h5>Estimated Price:</h5></div>
-            <div className='clear'><a href='#' onClick={this.clearState}>clear </a></div>
-          </div>
+          <FloatingPanel store={this.store} />
+
         </div>
       </div>
     )
