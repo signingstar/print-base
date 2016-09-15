dd880e6716531d6ce6ac07c92828f84ef06f1998
Anil <anilkeshari@gmail.com>
2016-09-16 00:17:59 +0530

    Move session related modules to external directory (#93)
    Move session related modules to external directory

---
+++ tests/client/sample.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('Session Helper', function() {
+  describe('#getUriWithCheck', () => {
+      it('should return all configs', () => {
+          expect(true).to.be.true;
+      });
+  });
+});
